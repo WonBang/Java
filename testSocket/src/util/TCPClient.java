21a39cabae79b2672c0ada56a9f3abddef09817f
wonbang <94_wonjun@naver.com>
2019-10-29 18:12:00 +0900

    Initial commit

---
+++ testSocket/src/util/TCPClient.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class TCPClient {
+    private static final String SERVER_IP = "192.168.56.1";
+    private static final int SERVER_PORT = 5000;
+    public static void main(String[] args){
+        Socket socket = null;
+
+        try{
+            socket = new Socket();
+
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            try{
+                if( socket != null && !socket.isClosed()){
+                    socket.close();
+                }
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
